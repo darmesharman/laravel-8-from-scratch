f2e986e77e7d0a1b990f497bea26e1ef853f5d44
Arman Darmesh <darmesharman@gmail.com>
2022-04-25 14:33:31 +0600

    Make a Route and Link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam illo culpa voluptates pariatur est itaque harum facere adipisci deserunt at quo tempore, laborum sequi cum magnam quae modi. Natus maxime quas possimus architecto eius, alias aperiam consequuntur! Dolor soluta quasi ad voluptate optio, magni facere praesentium, cupiditate nobis, quam eligendi.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dicta molestias eligendi quis nobis maxime. Quo placeat aspernatur nihil, illum incidunt repudiandae unde ipsam rem? Ipsa natus provident ducimus, iure dolorem animi corporis officiis quae numquam sunt necessitatibus ad recusandae, culpa at repellat, eveniet reprehenderit ullam itaque eos beatae ab?</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dicta molestias eligendi quis nobis maxime. Quo placeat aspernatur nihil, illum incidunt repudiandae unde ipsam rem? Ipsa natus provident ducimus, iure dolorem animi corporis officiis quae numquam sunt necessitatibus ad recusandae, culpa at repellat, eveniet reprehenderit ullam itaque eos beatae ab?</p>
+    </article>
+</body>
