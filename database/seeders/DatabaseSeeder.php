255c9b4182b6f692b27921fe5643089cd23e4428
Arman Darmesh <Arman.Darmesh@sberbank.kz>
2022-04-26 15:18:58 +0600

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,43 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
         $user = User::factory()->create();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, dolorem ratione animi dignissimos neque qui aliquam. Ad incidunt eaque quasi dolores a mollitia. Ducimus impedit similique alias explicabo molestiae harum. Sapiente autem assumenda laudantium rem reiciendis accusamus incidunt repellat maxime ut. Adipisci architecto, error impedit inventore voluptates saepe minima aspernatur!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, dolorem ratione animi dignissimos neque qui aliquam. Ad incidunt eaque quasi dolores a mollitia. Ducimus impedit similique alias explicabo molestiae harum. Sapiente autem assumenda laudantium rem reiciendis accusamus incidunt repellat maxime ut. Adipisci architecto, error impedit inventore voluptates saepe minima aspernatur!</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
