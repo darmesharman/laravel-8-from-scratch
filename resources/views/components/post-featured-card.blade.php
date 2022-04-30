2f40274d1e5ad3dd44f3183601b4314b01ff71ce
Arman Darmesh <darmesharman@gmail.com>
2022-05-01 01:05:21 +0600

    Author Filtering

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -12,8 +12,8 @@
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
                     <a href="/categories/{{ $post->category->slug }}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category->name }}</a>
+                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                       style="font-size: 10px">{{ $post->category->name }}</a>
                 </div>
 
                 <div class="mt-4">
@@ -37,14 +37,18 @@
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
-                        <h5 class="font-bold">{{ $post->author->name }}</h5>
+                        <h5 class="font-bold">
+                            <a href="?author={{ $post->author->username }}">
+                                {{ $post->author->name }}
+                            </a>
+                        </h5>
                         <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
 
                 <div class="hidden lg:block">
                     <a href="/posts/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                     >Read More</a>
                 </div>
             </footer>
