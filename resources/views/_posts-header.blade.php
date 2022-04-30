f3d8ee18831108ffb273ff41617402c6bf7647e0
Arman Darmesh <darmesharman@gmail.com>
2022-04-30 21:04:49 +0600

    Search (The Messy Way)

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -1,4 +1,3 @@
-
 <header class="max-w-xl mx-auto mt-20 text-center">
     <h1 class="text-4xl">
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
@@ -9,10 +8,11 @@
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
             <x-dropdown>
                 <x-slot name="trigger">
-                    <button class="block py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                    <button
+                        class="block py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
-                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px" />
+                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px"/>
                     </button>
                 </x-slot>
 
@@ -28,32 +28,36 @@
         </div>
 
         <!-- Other Filters -->
-{{--        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">--}}
-{{--            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
-{{--                <option value="category" disabled selected>Other Filters--}}
-{{--                </option>--}}
-{{--                <option value="foo">Foo--}}
-{{--                </option>--}}
-{{--                <option value="bar">Bar--}}
-{{--                </option>--}}
-{{--            </select>--}}
+        {{--        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">--}}
+        {{--            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
+        {{--                <option value="category" disabled selected>Other Filters--}}
+        {{--                </option>--}}
+        {{--                <option value="foo">Foo--}}
+        {{--                </option>--}}
+        {{--                <option value="bar">Bar--}}
+        {{--                </option>--}}
+        {{--            </select>--}}
 
-{{--            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"--}}
-{{--                    height="22" viewBox="0 0 22 22">--}}
-{{--                <g fill="none" fill-rule="evenodd">--}}
-{{--                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
-{{--                    </path>--}}
-{{--                    <path fill="#222"--}}
-{{--                            d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>--}}
-{{--                </g>--}}
-{{--            </svg>--}}
-{{--        </div>--}}
+        {{--            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"--}}
+        {{--                    height="22" viewBox="0 0 22 22">--}}
+        {{--                <g fill="none" fill-rule="evenodd">--}}
+        {{--                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
+        {{--                    </path>--}}
+        {{--                    <path fill="#222"--}}
+        {{--                            d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>--}}
+        {{--                </g>--}}
+        {{--            </svg>--}}
+        {{--        </div>--}}
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
-                        class="bg-transparent placeholder-black font-semibold text-sm">
+                <input type="text"
+                       name="search"
+                       placeholder="Find something"
+                       class="bg-transparent placeholder-black font-semibold text-sm"
+                       value="{{ request('search') }}"
+                >
             </form>
         </div>
     </div>
