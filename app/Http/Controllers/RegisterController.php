b852494e76534f617d3fd4a3b47ac363d93260e9
Arman Darmesh <darmesharman@gmail.com>
2022-05-02 23:45:07 +0600

    Login and Logout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use function Symfony\Component\String\s;
 
 class RegisterController extends Controller
 {
@@ -23,7 +20,9 @@ class RegisterController extends Controller
             'password' => ['required', 'min:7', 'max:255']
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        auth()->login($user);
 
         return redirect('/')->with('success', 'Your account has been created.');
     }
