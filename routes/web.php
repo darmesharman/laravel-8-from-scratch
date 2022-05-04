504382c811341e7d479a0668f64e24af47839e82
Arman Darmesh <darmesharman@gmail.com>
2022-05-05 04:33:54 +0600

    Mailchimp API Tinkering

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,19 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us18'
+    ]);
+
+    $response = $mailchimp->lists->getList('048a0a3e49');
+
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
